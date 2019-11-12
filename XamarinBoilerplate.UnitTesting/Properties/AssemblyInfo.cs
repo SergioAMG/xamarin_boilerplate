2539f3961e0a0a8616e9a852383c8385a26c7ea2
Sergio Martinez <Sergio_Martinez@epam.com>
2019-11-12 12:45:44 -0600

    Unit Testing project has been added to the solution in order to enable the testing on the Net standard project.

---
+++ XamarinBoilerplate.UnitTesting/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("XamarinBoilerplate.UnitTesting")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("EPAM Systems")]
+[assembly: AssemblyProduct("XamarinBoilerplate.UnitTesting")]
+[assembly: AssemblyCopyright("Copyright © EPAM Systems 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0a95a09c-de65-45e4-ae27-d9d11ac2c177")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
